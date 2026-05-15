6165257dbe07f1d08e5de2938466f06ce7977b0f
Krrish Tapwal <krrishtapwal1212@gmail.com>
2026-05-15 12:53:58 +0530

    Revert "Complete portfolio redesign: Next.js 16 + Framer Motion dark premium UI"
    This reverts commit acb2d577f006962c84312aae342b273a5f898020.

--- _next/static/chunks/0o0axo5o_38n9.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,39917,t=>{t.v(e=>Promise.all(["static/chunks/0pv8k8l4eb-m-.js"].map(e=>t.l(e))).then(()=>e(64756)))}]);
